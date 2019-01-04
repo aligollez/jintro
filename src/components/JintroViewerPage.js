0822381e9ca07b624422fb5a7d300a7436045215
Paul G <paulo.guetan@gmail.com>
2019-01-04 20:11:43 +0800

    Fix viewer countdown timer.

--- src/components/JintroViewerPage.js
+++ src/components/JintroViewerPage.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Countdown from 'react-countdown-now';
+import CountDown from '../components/CountDown';
 
 const JintroViewerPage = (props) => {
-  console.log(props);
-  
-  const Completionist = () => <a href={props.jintro.destinationUrl} className="destination-url">Click here to skip this ad</a>;
-  const renderer = ({ seconds, completed }) => {
-    if (completed) {
-      return <Completionist />;
-    } else {
-      return <span className="timer">Skip this ad in {seconds}</span>;
-    }
-  }
+  // console.log(props);
 
   const RenderJintro = () => {
     if (props.jintro) {
       return (      
         <div className="page-view">
           <div className="viewer-top-bar">
-            <Countdown date={Date.now() + 5000} renderer={renderer} />
+            <CountDown jintro={props.jintro} />
           </div>
           <iframe width="100%" frameBorder="0" height="100%" src={props.jintro.offerPageUrl} className="viewer-iframe"></iframe>
         </div>          
